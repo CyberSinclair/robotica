8a974b577ad1b510e5efa0b92f5f74fe7a865b70
David Sinclair <64096583+CyberSinclair@users.noreply.github.com>
2024-08-01 13:31:12 +0100

    added sonar Collison detection to avoid collisions

--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
-basic.showIcon(IconNames.Yes)
+let sonar2 = 0
+basic.showIcon(IconNames.Happy)
+cuteBot.forward()
 basic.forever(function () {
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(100, 40)
-    basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.forward()
-    basic.pause(200)
-    cuteBot.motors(40, 100)
-    basic.pause(1000)
-    cuteBot.forward()
-    basic.pause(200)
+    sonar2 = cuteBot.ultrasonic(cuteBot.SonarUnit.Centimeters)
+    if (sonar2 > 2 && sonar2 < 15) {
+        cuteBot.motors(0, 0)
+        for (let index = 0; index < 4; index++) {
+            music.play(music.tonePlayable(587, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+            basic.pause(2000)
+            cuteBot.motors(randint(-50, -100), -100)
+            basic.pause(500)
+        }
+    } else {
+        cuteBot.forward()
+    }
 })
